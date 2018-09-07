cf055a84a695080841bf53de3fa1904e9f2b5fd2
Vladislav <klychik-nsk54rus@mail.ru>
2018-09-08 00:16:45 +0700

    добавление искусственной вязкости
    используется формула скорости распространения продольной волны, поведение частиц более корректно

--- ComputeViscocity.m
+++ ComputeViscocity.m
@@ -1,14 +1,14 @@
-function [viscocity]=ComputeViscocity(x,v,rho,i,j,E,h)
+function [viscocity]=ComputeViscocity(x,v,rho,i,j,h)
     
     %E- elastic modulus for materials
-	
-    r_ab=x(1:2,i)-x(1:2,j);
-    v_ab=v(1:2,i)-v(1:2,j);
+	E=5;
+    r_ab=x(:,i)-x(:,j);
+    v_ab=v(:,i)-v(:,j);
     
     rho_a=rho(i);
     rho_b=rho(j);
     
-	if v_ab* r_ab>0
+	if dot(v_ab,r_ab)>0
 		viscocity=0;
     else
         alpha=1;   
@@ -16,12 +16,12 @@ function [viscocity]=ComputeViscocity(x,v,rho,i,j,E,h)
         h_ab=h;
         
         ro_ab=(rho_a+rho_b)/2;
-        cs_a=sqrt(E/rho_a);
+        cs_a=sqrt(E/rho_a); %скорость продольной волны
         cs_b=sqrt(E/rho_b);
         c_ab=(cs_a+ cs_b)/2;
         
         nu=h_ab*0.1;
-        mu=(h*v_ab*r_ab) / (r_ab^2 + nu^2);
+        mu=(h*dot(v_ab,r_ab)) / (dot(r_ab,r_ab) + nu^2);
         
         viscocity=(-alpha*c_ab*mu + betta*mu*mu) / ro_ab;
     end
