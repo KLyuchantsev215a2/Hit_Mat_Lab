17002f8a48a273c7469f757e58e6af54111e3b8b
Vladislav <klychik-nsk54rus@mail.ru>
2018-09-13 19:58:45 +0700

    Баги
    Забыл суммирование в тензоре градиента скорости, графики теперь выводят только вержнию пластину

--- initialization_v.m
+++ initialization_v.m
@@ -1,12 +1,12 @@
-function [init] =initialization_v(v_0,N)
+function [init] =initialization_v(v_0,N,sqn)
 
 v=zeros(1,2,N);
-    for i=1:25
+    for i=1:(sqn*sqn)
         v(1,1,i)=0;
         v(1,2,i)=-v_0;
     end
     
-    for i=26:50
+    for i=(sqn*sqn+1):sqn*sqn*2
         v(1,1,i)=0;
         v(1,2,i)=v_0;
     end
--- initialization_x.m
+++ initialization_x.m
@@ -1,4 +1,4 @@
-function [init] =initialization_x(flag,N) %initialization(L,H,N,flag)
+function [init] =initialization_x(flag,N,sqn) %initialization(L,H,N,flag)
 %for now size fixed (2,2)
 %flag 1 -upper plate -1 -lower plate
 %N_y = fix(sqrt(N*L/H));
@@ -6,16 +6,16 @@ function [init] =initialization_x(flag,N) %initialization(L,H,N,flag)
 i=1;
 x=zeros(1,2,N);
 
-for  xi=1:5
-    for  yi=1:5
+for  xi=1:sqn
+    for  yi=1:sqn
         x(1,1,i) =xi;
         x(1,2,i) =yi;
         i=i+1;
     end
 end
 
-for  xi=1:5
-    for  yi=1:5
+for  xi=1:sqn
+    for  yi=1:sqn
         x(1,1,i) =xi*flag;
         x(1,2,i) =-yi*flag;
         i=i+1;
